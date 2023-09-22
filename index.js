13841b21c70101d9d298a80cacac4ce4fa1a9b26
freelancershrawani <141408350+freelancershrawani@users.noreply.github.com>
2023-09-22 15:58:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+     $("#name").text(profile.getName());
+     $("#email").text(profile.getEmail());
+     $("#image").attr('src',profile.getImageUrl());
+     $(".data").css("display","block");
+     $(".g-signin2").css("display","none");
+  }
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+       alert("You have been successfully signed out");
+       $(".g-signin2").css("display","block");
+       $(".data").css("display","none");
+    });
+}
